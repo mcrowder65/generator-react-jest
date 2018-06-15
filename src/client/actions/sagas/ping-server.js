59e579a3359026e7f4e356d41445462369c3b328
Matt Crowder <matt.crowder@walmart.com>
2018-06-15 13:04:41 -0400

    adding some server stuff

--- src/client/actions/sagas/ping-server.js
+++ src/client/actions/sagas/ping-server.js
@@ -2,9 +2,10 @@ import { call, put } from "redux-saga/effects";
 
 import { setPing } from "../index";
 
+const url = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
 export const apiCall = async () => {
   // this is up to you whether or not you want to implement this server...
-  const res = await fetch("http://localhost:3000/ping", { method: "GET" });
+  const res = await fetch(`${url}/ping`, { method: "GET" });
   return res.text();
 };
 
--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,15 @@ import bodyParser from "body-parser";
 import path from "path";
 
 const app = express();
-
+if (process.env.NODE_ENV === "development") {
+  // eslint-disable-next-line no-console
+  console.log("CORs enabled");
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("*.js", (req, res, next) => {
@@ -11,6 +19,9 @@ app.get("*.js", (req, res, next) => {
   req.url += ".gz";
   res.set("Content-Encoding", "gzip");
   next();
+});
+app.get("/ping", (req, res) => {
+  res.send(`The server says hello`);
 });
 app.use(express.static(path.resolve(__dirname, "../..", "build")));
 // eslint-disable-next-line no-console
