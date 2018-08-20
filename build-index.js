8dda0c17ab16a85284265f787bf78c6a42ef4075
Matt Crowder <mcrowder65@gmail.com>
2018-08-20 19:14:27 -0400

    config

--- build-index.js
+++ build-index.js
@@ -127,7 +127,7 @@ var cli = function cli() {
                         "server-watch": "NODE_ENV=development && babel-watch src/server/index.js"
                       }),
                       jest: (0, _extends4.default)({}, pkgJson.jest, {
-                        setupTestFrameworkScriptFile: "<rootDir>/test/client/config.js",
+                        setupTestFrameworkScriptFile: "<rootDir>/test/config.js",
                         moduleNameMapper: {
                           "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
                           "\\.(css|scss|less)$": "identity-obj-proxy"
--- index.js
+++ index.js
@@ -178,7 +178,7 @@ const cli = () => {
             },
             jest: {
               ...pkgJson.jest,
-              setupTestFrameworkScriptFile: "<rootDir>/test/client/config.js",
+              setupTestFrameworkScriptFile: "<rootDir>/test/config.js",
               moduleNameMapper: {
                 "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
                   "<rootDir>/__mocks__/file-mock.js",
